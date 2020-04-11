91b1a67ee63b2d04c24cb0ee31e348a272e0c5e7
Nik Cubrilovic <nc9@protonmail.com>
2020-04-11 23:39:30 +1000

    REFACTOR: Refector user into request_user_from_session

---
+++ beatcovid/respondent/controllers.py
@@ -0,0 +1,40 @@
+import logging
+import uuid
+
+from .models import Respondent, Session
+
+logger = logging.getLogger(__name__)
+
+
+def get_user_from_request(request):
+    """
+        Retrieves the user from the session
+
+    """
+    user_id = request.session.get("user", None)
+    session_key = request.session._get_or_create_session_key()
+
+    u = None
+    s = None
+
+    if user_id:
+        user_id = uuid.UUID(hex=user_id)
+        u = Respondent.objects.get(id=user_id)
+        logger.debug(f"Retrieved user {u.id}")
+
+    if not u:
+        u = Respondent()
+        u.save()
+        request.session["user"] = user.id.hex
+        logger.debug(f"Created new user {u.id}")
+
+    if session_key:
+        s = Session.objects.filter(cookie_id=session_key).first()
+
+        if not s:
+            s = Session()
+            s.cookie_id = session_key
+            s.respondent = u
+            s.save()
+
+    return s.respondent
--- beatcovid/respondent/views.py
+++ beatcovid/respondent/views.py
@@ -1,48 +1,17 @@
 import logging
-import uuid
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Respondent, Session
+from .controllers import get_user_from_request
 
 logger = logging.getLogger(__name__)
 
 
-def get_user(user_id, session_key):
-    u = None
-    s = None
-
-    if user_id:
-        user_id = uuid.UUID(hex=user_id)
-        u = Respondent.objects.get(id=user_id)
-        logger.debug(f"Retrieved user {u.id}")
-
-    if not u:
-        u = Respondent()
-        u.save()
-        logger.debug(f"Created new user {u.id}")
-
-    if session_key:
-        s = Session.objects.filter(cookie_id=session_key).first()
-
-        if not s:
-            s = Session()
-            s.cookie_id = session_key
-            s.respondent = u
-            s.save()
-
-    return s.respondent
-
-
 @api_view(["GET"])
 def UserDetailView(request):
-    user = get_user(
-        request.session.get("user", None), request.session._get_or_create_session_key()
-    )
-
-    request.session["user"] = user.id.hex
+    user = get_user_from_request(request)
 
     # @TODO include this in the form response
     return Response({"user": str(user.id)})
