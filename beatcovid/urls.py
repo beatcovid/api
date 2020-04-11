1c36a89bbb814466f40d6527a20ea8d6e837b408
Nik Cubrilovic <nc9@protonmail.com>
2020-04-11 20:15:15 +1000

    Merge all URL patterns in to project root rather than using dynamic loading

--- beatcovid/urls.py
+++ beatcovid/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from beatcovid.api.views import FormData, FormSchema, FormStats, FormSubmission
+from beatcovid.respondent.views import UserDetailView
+
 admin.site.site_header = "beatcovid19 Admin"
 admin.site.site_title = "beatcovid19 Admin"
 admin.site.index_title = "beatcovid19 Admin"
 
 urlpatterns = [
     path("api/", include("beatcovid.api.urls")),
-    path("user/", include("beatcovid.respondent.urls")),
+    path("api/form/schema/<str:form_name>/", FormSchema),
+    path("api/form/stats/<str:form_name>/", FormStats),
+    path("api/form/submit/<str:form_name>/", FormSubmission),
+    path("api/form/data/<str:form_name>/", FormData),
+    path("user", UserDetailView.as_view()),
     # path("auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
 ]
