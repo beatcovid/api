079b5b3c38d99437f2e2fc21219762773b06c0f4
Nik Cubrilovic <nc9@protonmail.com>
2020-04-11 20:27:07 +1000

    Serve staticfiles from Django for now

--- beatcovid/urls.py
+++ beatcovid/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -17,4 +19,4 @@ urlpatterns = [
     path("user", UserDetailView.as_view()),
     # path("auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
