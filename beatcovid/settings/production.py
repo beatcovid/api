5fc0f12a029121f64c49b1155e36f7be9ba1ac64
Nik Cubrilovic <nc9@protonmail.com>
2020-04-18 21:34:36 +1000

    Setup production sentry

--- beatcovid/settings/production.py
+++ beatcovid/settings/production.py
@@ -1,4 +1,17 @@
 
-from .base import *
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 SESSION_COOKIE_SECURE = True
+
+SENTRY_DSN = "https://6330206ccb7f40dfa6d9ef63174f96ba@o368785.ingest.sentry.io/5204925"
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[DjangoIntegration()],
+    environment="production",
+    send_default_pii=True,
+)
+
+from .base import *  # isort:skip
