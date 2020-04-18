7955907d6842433500e0d962ced3022bae70ce8c
Nik Cubrilovic <nc9@protonmail.com>
2020-04-18 21:10:11 +1000

    Serialize kobo readable dicts

---
+++ beatcovid/symtracker/survey_serializer.py
@@ -0,0 +1,8 @@
+
+def from_readable_dict(d):
+    """
+        Kobo has a format known as a "readable dict" where it
+        flattens out keys for embedded dicts into name/name/name
+        format. Here we unpack it.
+    """
+    pass
