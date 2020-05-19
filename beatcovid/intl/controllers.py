111d6960336948ab614dcceb2c91a1746f136578
Nik Cubrilovic <nc9@protonmail.com>
2020-05-20 06:23:39 +1000

    Parse po files

--- beatcovid/intl/controllers.py
+++ beatcovid/intl/controllers.py
@@ -2,6 +2,7 @@ import glob
 import os
 from pprint import pprint
 
+import polib
 from django.core.management.base import CommandError
 from django.core.management.utils import find_command, is_ignored_path
 
@@ -43,20 +44,50 @@ def find_locale_paths():
         locale_dirs = filter(os.path.isdir, glob.glob("%s/en" % basedir))
         locales.extend(map(os.path.basename, locale_dirs))
 
+    locations = []
+
     for basedir in basedirs:
         if locales:
             dirs = [os.path.join(basedir, l, "LC_MESSAGES") for l in locales]
         else:
             dirs = [basedir]
-        locations = []
         for ldir in dirs:
             for dirpath, dirnames, filenames in os.walk(ldir):
                 locations.extend((dirpath, f) for f in filenames if f.endswith(".po"))
-        if locations:
-            pprint(locations)
-            # self.compile_messages(locations)
+
+    return locations
 
 
 def parse_locales(locations):
+    locs = set()
+    msgstrings = []
+
     for i, (dirpath, f) in enumerate(locations):
-        po_path = os.path.join(dirpath, f)
+        locs.add(os.path.join(dirpath, f))
+
+    for po_path in locs:
+        po = polib.pofile(po_path)
+        print(f"opening {po_path}")
+        valid_entries = [e for e in po if not e.obsolete]
+        for entry in valid_entries:
+            key = po_to_lokalise(entry)
+            msgstrings.append(key)
+
+    return msgstrings
+
+
+def po_to_lokalise(poentry):
+    floc = []
+
+    for (f, l) in poentry.occurrences:
+        floc.append("{}:{}".format(f, l))
+
+    occurrences = "\n".join(floc)
+
+    return {
+        "key_name": poentry.msgid,
+        "comments": {"comment": poentry.comment or occurrences},
+        "platforms": ["web"],
+        "tags": ["api"],
+        "translations": [{"language_iso": "en", "translation": poentry.msgstr}],
+    }
