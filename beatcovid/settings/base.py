ca4c4f57261ac3cbd2d1e5a95960eb38873d3313
Nik Cubrilovic <nc9@protonmail.com>
2020-05-26 01:49:38 +1000

    Remove old code comments

--- beatcovid/settings/base.py
+++ beatcovid/settings/base.py
@@ -164,8 +164,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-        # @TODO we'll proably need this
-        # "rest_framework.authtoken",
     ]
 }
 
